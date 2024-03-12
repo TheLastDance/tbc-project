83d51c112887b7737edf2e04d54df20b682567ac
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-03-13 03:30:38 +0400

    adding Blog components
    - created and styled Blog components
    - updated few styles for other components

--- src/components/BlogList/BlogList.jsx
+++ src/components/BlogList/BlogList.jsx
@@ -1,7 +1,21 @@
-"./BlogList.css"
+import "./BlogList.css";
+import { Post } from "./Post/Post";
+import { blogPosts } from "../../data/blogPosts";
 
 export function BlogList() {
+  const { posts } = blogPosts;
+
   return (
-    <div>BlogList</div>
+    <section className="blog">
+      <h2>Blog:</h2>
+      <ul>
+        {posts.map((item) =>
+          <Post
+            key={item.id}
+            item={item}
+          />
+        )}
+      </ul>
+    </section>
   )
 }
