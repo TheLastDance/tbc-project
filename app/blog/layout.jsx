297fc4ea013de2a05ece914d7b78da9922f09d8b
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-04-02 05:50:32 +0400

    adding metadata for all pages
    - added metadata for pages
    - applied test icon for metadata

---
+++ app/blog/layout.jsx
@@ -0,0 +1,11 @@
+
+export const metadata = {
+  title: "Blog 🧾",
+  description: "Dive into a world of insights and stories. Our blog features articles on a wide range of topics, offering thought-provoking perspectives and valuable information.",
+};
+
+export default function layout({ children }) {
+  return (
+    <>{children}</>
+  )
+}
---
+++ app/contact/layout.jsx
@@ -0,0 +1,11 @@
+
+export const metadata = {
+  title: "Contact Us 📞",
+  description: "Get in touch with our team for assistance, questions, or feedback. We're here to help and listen to your needs.",
+};
+
+export default function layout({ children }) {
+  return (
+    <>{children}</>
+  )
+}
