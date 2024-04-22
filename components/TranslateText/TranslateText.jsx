d4a43538f3bb4824b9dfa6d448659af7d58b022b
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-04-22 18:50:00 +0400

    refactoring code
    - fixed few translations
    - added fallback for TranslateText component
    - added size prop for Loader component
    - fixed color of Card component border on dark mode

--- components/TranslateText/TranslateText.jsx
+++ components/TranslateText/TranslateText.jsx
@@ -3,11 +3,12 @@
 import { useContext } from "react";
 import { languageContext } from "@/services/providers/LanguageProvider";
 import { translations } from "@/translations/translations";
+import { Loader } from "../Loaders/Loader/Loader";
 
 export function TranslateText({ translationKey }) {
   const { locale } = useContext(languageContext);
 
-  if (!locale) return "Loading...";
+  if (!locale) return <Loader size="18px" />;
 
   return translations[locale][translationKey]
 }
