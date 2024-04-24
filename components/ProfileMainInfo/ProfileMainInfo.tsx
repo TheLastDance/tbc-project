89554d0253105eeeea19d1afc3ee40e3dcb916f4
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-04-25 00:05:19 +0400

    Partial Migration to Typescript

--- components/ProfileMainInfo/ProfileMainInfo.tsx
+++ components/ProfileMainInfo/ProfileMainInfo.tsx
@@ -1,4 +1,4 @@
-import "./ProfileMainInfo.css"
+import "./ProfileMainInfo.css";
 import Image from "next/image";
 import profile_img from "@/public/img/profile/albert_avatar.jpg";
 
@@ -6,13 +6,7 @@ export function ProfileMainInfo() {
   return (
     <div className="profileMainInfo_card">
       <h1>Michael Brown</h1>
-      <Image
-        src={profile_img}
-        alt="profile avatar"
-        width={300}
-        height={380}
-        priority
-      />
+      <Image src={profile_img} alt="profile avatar" width={300} height={380} priority />
     </div>
-  )
+  );
 }
